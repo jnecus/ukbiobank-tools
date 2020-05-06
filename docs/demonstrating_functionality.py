1c1e0726611dad209c80140fd9b98527e725e0f4
jnecus <j.necus2@ncl.ac.uk>
2020-05-06 13:03:24 +0100

    Update demonstrating_functionality.py

--- docs/demonstrating_functionality.py
+++ docs/demonstrating_functionality.py
@@ -29,11 +29,6 @@ fieldnames=getFieldnames(ukb)
 #loading csv with fields
 #df=loadCsv(ukb,fields=['eid','Non-cancer illness code, self-reported','Date of all cause dementia report','Source of all cause dementia report', 'Diagnoses - main ICD10','Diagnoses - main ICD9'])
 
-#loading a sample of df for testing purposes
-df=loadSampleCsv(ukb,fields=['eid','Non-cancer illness code, self-reported','Date of all cause dementia report','Source of all cause dementia report', 'Diagnoses - main ICD10','Diagnoses - main ICD9'],n_rows=2000)
-
-
-
 
 
 
@@ -67,62 +62,6 @@ df5=illnessCodesToText(ukb, df4)
 
 """ To Do"""
 #df6=addFields(ukb, df, fields=[])
-
-""" FYI Speed test results ZIP vs UNZIpped 
-
-Time taken to load 6 fields using ukb41344 Zipped  :  92.65625
-Time taken to load 6 fields using ukb41344 csv  :  66.484375
-
-Time taken to load 6 fields using ukb41831 Zipped  :  192.5
-Time taken to load 6 fields using ukb41831 csv  :  143.125
-
-
-Time taken to add a single field using ukb41344 Zipped  :  51.25
-Time taken to add a single field using ukb41344 csv  :  34.625
-
-Time taken to add a single field using ukb41831 Zipped  :  121.96875
-Time taken to add a single field using ukb41831 csv  :  83.734375
-
-Time taken to load ukb41344 Zipped ukb object 8.625
-Time taken to load ukb41344 csv ukb object 6.859375
-Time taken to load ukb41831 Zipped ukb object 17.828125
-Time taken to load ukb41831 csv ukb object 17.71875
-
-
-"""
-
-
-"""
-#Questions to address (this process will raise useful functionality)
-
-1. How many people have a diagnosis of dementia and one scan or two scans?
-
-1.5. What is the duration of time between diagnosis of dementia and scan?
-
-2. How many people have record of dementia & depression at time of any scan?
-
-3. What list of variables are useful for quantifying cognitive function? physical function? Dementia?
-
-4. Is there a relationship between time from diagnosis  -> Instance 0 1 2 or 3 and brain/cognitive/physical function at Instance  0 1 2 or 3
-
-"""
-
-
-#1. How many people have a diagnosis of dementia and one scan or two scans?
-
-
-#Generating 'Date of all cause dementia report' in separate script : 'generatingAllCauseDementiaReport20200401.py'
-
-
-
-
-
-
-
-
-
-
-
 
 
 
