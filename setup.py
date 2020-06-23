a4b44360cfd756e5669c5354398fb1523f5f7dce
jnecus <joe.necus@ncl.ac.uk>
2020-06-23 22:19:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def list_requirements():
 	
 setup(
     name='ukbiobank-tools',
-    version='0.1.92',
+    version='0.1.93',
     description='Useful tools for working with UK Biobank meta-data',
     author='Joe Necus',
     author_email='joenecus@gmail.com',
