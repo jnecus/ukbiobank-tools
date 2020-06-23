346c15c24d1bdd9b8e7e5572f75e79680d4f7019
jnecus <joe.necus@ncl.ac.uk>
2020-06-23 22:19:15 +0100

    setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     license=license,	
 	packages=find_packages(include=["ukbiobank"]),
     package_data={"": ["data_coding/*"]},
-	install_requires=["pandas"],
+	install_requires=["pandas", "wxpython"],
     extras_require = {"docs": [ "sphinx",
                                 "sphinxcontrib-httpdomain",
                                 "sphinx-rtd-theme"]})
